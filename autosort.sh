43dafb67f1f6e216cdae8e6e698589212ba4361f
Karl Herrick <me@karlherrick.com>
2017-02-23 23:35:22 -0500

    refactor and cleanup
    * use printf over echo
    * use two spaces instead of tabs
    * use two square brackets for tests
    * update shebang to be more portable

--- autosort.sh
+++ autosort.sh
@@ -1,38 +1,48 @@
-#!/bin/bash
+#!/usr/bin/env bash
+
 function usage() {
-	echo
-	echo Usage: $0 [DIRECTORY]
-	echo
-	echo Example: $0 ~/files
-	exit 1
+  printf '\n'
+  printf 'Usage: %s [DIRECTORY]\n' "$0"
+  printf '\n'
+  printf 'Example: %s ~/files\n' "$0"
+
+  exit 1
 }
 
-if [ "$1" = "" ]; then
-	echo
-	echo "No argument specified."
-	usage
+if [[ "$1" == '' ]]; then
+  printf '\n'
+  printf 'No argument specified.\n'
+
+  usage
 else
-	if [ ! -d "$1" ]; then
-		echo
-		echo "Directory doesn't exist."
-		usage
-	fi
-	DIRECTORY=$1
-	SAVEIFS=$IFS
-	IFS=$(echo -en "\n\b")
-	for FILE in `find $DIRECTORY -maxdepth 1 -type f -exec ls -1 {} \;`; do
-		#don't move this script, if it is in the current directory
-		if [ "$FILE" = "$0" ]; then 
-			continue 
-		fi
-		FILEISODATE=`ls -l --full-time "$FILE" | cut -d" " -f 6`
-		if [ ! -d "$DIRECTORY/$FILEISODATE" ]; then
-			echo Making the \"$DIRECTORY/$FILEISODATE\" directory.
-			echo
-			mkdir "$DIRECTORY/$FILEISODATE"
-		fi
-		echo Moving "$FILE" to "$DIRECTORY/$FILEISODATE"...
-		echo
-		mv "$FILE" "$DIRECTORY/$FILEISODATE"
-	done
+  if [[ ! -d "$1" ]]; then
+    printf '\n'
+    printf "Directory doesn't exist.\n"
+
+    usage
+  fi
+
+  readonly DIRECTORY="$1"
+  IFS=$(echo -en "\n\b")
+  for FILE in $(find "$DIRECTORY" -maxdepth 1 -type f -exec ls -1 {} \;); do
+    #don't move this script, if it is in the current directory
+    if [[ "$FILE" = "$0" ]]; then
+      continue;
+    fi
+
+    declare FILEISODATE
+    FILEISODATE=$(ls -l --full-time "$FILE" | cut -d" " -f 6)
+
+    if [ ! -d "$DIRECTORY/$FILEISODATE" ]; then
+      printf 'Making the "%s/%s" directory.' "$DIRECTORY" "$FILEISODATE"
+      printf '\n'
+
+      mkdir "$DIRECTORY/$FILEISODATE"
+    fi
+
+    printf 'Moving "%s" to "%s/%s"...' "$FILE" "$DIRECTORY" "$FILEISODATE"
+    printf '\n'
+
+    mv "$FILE" "$DIRECTORY/$FILEISODATE"
+  done
 fi
