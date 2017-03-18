f86862d87a5ee69448fffd972f3bca50890f41ff
Karl Herrick <me@karlherrick.com>
2017-03-17 20:55:33 -0400

    reduce nesting

--- autosort.sh
+++ autosort.sh
@@ -31,49 +31,49 @@ if [[ "$DIRECTORY" == '' ]]; then
   printf '\nNo argument specified.\n'
 
   usage
-else
-  if [[ ! -d "$DIRECTORY" ]]; then
-    printf "\nDirectory doesn't exist.\n"
+fi
 
-    usage
+if [[ ! -d "$DIRECTORY" ]]; then
+  printf "\nDirectory doesn't exist.\n"
+
+  usage
+fi
+
+for FILE in "$DIRECTORY"/*; do
+  # there are no files to process
+  if [[ "$FILE" == "$DIRECTORY/*" ]]; then
+    continue;
   fi
 
-  for FILE in "$DIRECTORY"/*; do
-    # there are no files to process
-    if [[ "$FILE" == "$DIRECTORY/*" ]]; then
-      continue;
-    fi
+  # don't move this script, if it is in the current directory
+  if [[ "$FILE" == "$0" ]]; then
+    continue;
+  fi
 
-    # don't move this script, if it is in the current directory
-    if [[ "$FILE" == "$0" ]]; then
-      continue;
-    fi
+  declare file_timestamp
+  declare file_iso_8601_date_string
 
-    declare file_timestamp
-    declare file_iso_8601_date_string
+  # determine stat support
+  if [[ $(isGnuStatAvailable) == 'true' ]]; then
+    file_timestamp=$(stat --format "%Y" "$FILE")
+  else
+    file_timestamp=$(stat -f "%m" "$FILE")
+  fi
 
-    # determine stat support
-    if [[ $(isGnuStatAvailable) == 'true' ]]; then
-      file_timestamp=$(stat --format "%Y" "$FILE")
-    else
-      file_timestamp=$(stat -f "%m" "$FILE")
-    fi
+  # determine date support
+  if [[ $(isGnuDateAvailable) == 'true' ]]; then
+    file_iso_8601_date_string=$(date -d "@$file_timestamp" +'%Y-%m-%d')
+  else
+    file_iso_8601_date_string=$(date -r "$file_timestamp" +'%Y-%m-%d')
+  fi
 
-    # determine date support
-    if [[ $(isGnuDateAvailable) == 'true' ]]; then
-      file_iso_8601_date_string=$(date -d "@$file_timestamp" +'%Y-%m-%d')
-    else
-      file_iso_8601_date_string=$(date -r "$file_timestamp" +'%Y-%m-%d')
-    fi
+  if [ ! -d "$DIRECTORY/$file_iso_8601_date_string" ]; then
+    printf 'Making the "%s/%s" directory.\n' "$DIRECTORY" "$file_iso_8601_date_string"
 
-    if [ ! -d "$DIRECTORY/$file_iso_8601_date_string" ]; then
-      printf 'Making the "%s/%s" directory.\n' "$DIRECTORY" "$file_iso_8601_date_string"
+    mkdir "$DIRECTORY/$file_iso_8601_date_string"
+  fi
 
-      mkdir "$DIRECTORY/$file_iso_8601_date_string"
-    fi
+  printf 'Moving "%s" to "%s/%s"...\n' "$FILE" "$DIRECTORY" "$file_iso_8601_date_string"
 
-    printf 'Moving "%s" to "%s/%s"...\n' "$FILE" "$DIRECTORY" "$file_iso_8601_date_string"
-
-    mv "$FILE" "$DIRECTORY/$file_iso_8601_date_string"
-  done
-fi
+  mv "$FILE" "$DIRECTORY/$file_iso_8601_date_string"
+done
